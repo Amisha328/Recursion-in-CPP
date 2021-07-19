4b0142a49da197e5681f5a99c85a8a24cb44350b
Amisha Sahu <amishasahu328@gmail.com>
2021-07-19 20:42:29 +0530

    Josephus problem statement

---
+++ JosephusProbelm.cpp
@@ -0,0 +1,2 @@
+// Problem statement:
+// Josephus problem is a mathematical game. There are n people numbered from 1 to n.People are sitting in a circle and are numbered from 1 to n in clockwise order. 
