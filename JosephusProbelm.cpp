8c519cce020cf209ec6075072ec5c145bb041eb0
Amisha Sahu <amishasahu328@gmail.com>
2021-07-19 20:43:15 +0530

    Josephus problem game rules

--- JosephusProbelm.cpp
+++ JosephusProbelm.cpp
@@ -1,2 +1,9 @@
 // Problem statement:
 // Josephus problem is a mathematical game. There are n people numbered from 1 to n.People are sitting in a circle and are numbered from 1 to n in clockwise order. 
+
+// Rules:
+// 1.Start at the 1st people.
+// 2.Count the next k people in the clockwise direction including the people you started at. The counting wraps around the circle and may count some people more than once.
+// 3.The last people you counted leaves the circle and loses the game.
+// 4.If there is still more than one people in the circle, go back to step 2 starting from the people immediately clockwise of the people who just lost and repeat.
+// 5.Else, the last people in the circle wins the game.
