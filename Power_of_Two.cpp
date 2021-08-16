f27e2609cdc952930117026e3b0ead84308954ed
Amisha Sahu <58816552+Amisha328@users.noreply.github.com>
2021-08-16 20:42:33 +0530

    power of two

---
+++ Power_of_Two.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define eff ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+bool isPowerOfTwo(int n)
+{
+      if(n>0){  
+       if(n == 0)
+         return false;
+     return ((n & (n - 1)) == 0); 
+    }
+    else
+        return false;
+}
+
+signed main(){
+    eff;
+    int n;
+    cin>>n;
+    if(isPowerOfTwo(n)) cout<<"It is a power of two";
+    else cout<<"It is not a power of two";
+    return 0;
+}
